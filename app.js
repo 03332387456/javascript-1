587ce245f871b846f690dcb599690f643bc76bf4
03332387456 <sahirmavia@gmail.com>
2023-03-30 11:39:20 +0500

    this is a first assignment of javascript

---
+++ app.js
@@ -0,0 +1,203 @@
+
+// alert("HELLO WORLD")
+// alert("this is a world ")
+// alert("this is a RAMZAN")
+
+
+    var a = "10";
+    var b = "70";
+    var c = "a-b"
+    console.log("c")
+
+    var a = 10;
+    var b = "70";
+    var c = a+b
+    console.log(c)
+
+
+    var d  = "name"
+    var e =  "AHSAN"
+    var f = d+e
+    console.log(f)
+
+
+
+    var g  = "10"
+    var h =  "50"
+    var i = g/h
+    console.log(i)
+
+
+    var a = 10;
+    var b = 70;
+    var c = a - b;
+    console.log(c);
+
+
+
+    var a = 10;
+    var b = "70";
+    var c = a+b
+    console.log(c)
+
+
+
+    var d  = "name"
+    var e =  "AHSAN"
+    var f = d+e
+    console.log(f)
+
+
+    var g  = "10"
+    var h =  "50"
+    var i = g*h
+    console.log(i)
+
+
+    // var ahjj = "agdh" legal
+    // var ajkj89 = "agdh" legal
+    // var nam-e = "jhfpk" illegal
+    // var $yu= "jhfpk" illegal 
+
+
+    var x = 10;
+    var y = 5;
+    var z = x + y; 
+    console.log(z)
+
+
+    var x = 10;
+    var y = 5;
+    var z = x - y; 
+    console.log(z)
+
+
+
+    var x = 10;
+    var y = 5;
+    var z = x * y; 
+    console.log(z)
+
+
+    var x = 10;
+    var y = 5;
+    var z = x / y; 
+    console.log(z)
+
+
+    var x = 'ahsan' 
+    var y = 'java' 
+    var z = x/y
+    console.log(z)
+
+
+var w = 10;
+var x = w++;
+console.log(w)
+
+// var e = 10
+// var f = e++ + ++e - e + e++ - ++e + e
+// var g = alert (f)
+
+
+var a = 4
+var b = 5 
+var c = 8
+var d = (a+b)*c
+console.log(d) 
+
+
+var e = 4
+var f = 5 
+var g = 8
+var h = (e*f)+h
+console.log(h) 
+
+
+
+ var message = "Thanks for your input!";
+ console.log(message);
+ 
+
+var str1 = "Hello";
+var str2 = "world";
+var result = str1+ " " + str2;
+console.log(result);
+
+
+var n1= "33";
+var n2 = "6";
+var result = n1+ " "  + " " +  " " +  " " +  " " + n2;
+console.log(result);
+
+
+// var firstname = prompt ("ENTER FIRST NAME ")
+// var lastname = prompt ("ENTER LAST NAME ")
+// var city = prompt ("ENTER CITY NAME ")
+// console.log( firstname+ " " +  lastname+ " " + city + " KARACHI "  ) 
+
+
+
+//  var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+ alert("Correct!");
+ }
+
+
+if( kalMuftaHa = false ){
+    console.log(" masjid ma ")
+} else { console.log ("nahi ha ") }
+
+
+
+if( todayisthe8thRoza  = true ){
+    console.log(" tomorrow is the 9th roza ")
+} else { console.log ("or 10th taravi ha") }
+
+
+console.log(3 == 3); 
+console.log(3 == "3"); 
+console.log(3 == 4); 
+
+console.log(5 === 5); 
+console.log(5 === "5"); 
+console.log(true === 1); 
+console.log(true === true); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
